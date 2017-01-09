e2c39e52c8d39c6188791fdffce12b83b2c6326a
Gleb Billig <gleb.billig@gmail.com>
2017-01-08 19:04:57 -0800

    Add input and output directory parameters

--- glbcpy.sh
+++ glbcpy.sh
@@ -3,5 +3,20 @@
 if [ "$1" = "-h" -o "$1" = "--help" ]
 then
 	echo "Help Message"
-	exit 0
+	echo "-i INPUT_DIR"
+	echo "-o OUTPUT_DIR"
+	exit 1
+fi
+
+if [ "$1" = "-i" -a "$3" = "-o" ]
+then
+	src=$2
+	dst=$4
+	if [ -d "$src" -a -d "$dst" ]
+	then
+		echo "ok copy"
+	else
+		echo "$src or $dst is not a valid directory"
+		exit 1
+	fi
 fi
